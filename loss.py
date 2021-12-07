a5a2c648595f071c9eee23f5c80fcb5d7d733b79
AnTao97 <taoan2008@live.cn>
2021-12-07 21:22:50 +0800

    fix https://github.com/AnTao97/UnsupervisedPointCloudReconstruction/issues/4

--- loss.py
+++ loss.py
@@ -20,16 +20,11 @@ class ChamferLoss(nn.Module):
     def batch_pairwise_dist(self, x, y):
         bs, num_points_x, points_dim = x.size()
         _, num_points_y, _ = y.size()
-        xx = torch.bmm(x, x.transpose(2, 1))
-        yy = torch.bmm(y, y.transpose(2, 1))
+        xx = x.pow(2).sum(dim=-1)
+        yy = y.pow(2).sum(dim=-1)
         zz = torch.bmm(x, y.transpose(2, 1))
-        diag_ind_x = torch.arange(0, num_points_x)
-        diag_ind_y = torch.arange(0, num_points_y)
-        if x.get_device() != -1:
-            diag_ind_x = diag_ind_x.cuda(x.get_device())
-            diag_ind_y = diag_ind_y.cuda(x.get_device())
-        rx = xx[:, diag_ind_x, diag_ind_x].unsqueeze(1).expand_as(zz.transpose(2, 1))
-        ry = yy[:, diag_ind_y, diag_ind_y].unsqueeze(1).expand_as(zz)
+        rx = xx.unsqueeze(1).expand_as(zz.transpose(2, 1))
+        ry = yy.unsqueeze(1).expand_as(zz)
         P = (rx.transpose(2, 1) + ry - 2 * zz)
         return P
 
